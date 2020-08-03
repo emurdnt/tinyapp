8384129dc29117a6c7aad74525c54e5ae0358dfd
Em Urdaneta <em.urdaneta94@gmail.com>
2020-08-03 21:17:53 +0000

    added shortURL path

--- express_server.js
+++ express_server.js
@@ -22,6 +22,14 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
+app.get("/urls/:shortURL", (req, res) => {
+  //let id = JSON.stringify(req.params.shortURL);
+  let templateVars = { shortURL: req.params.shortURL, longURL:urlDatabase[req.params.shortURL]};
+  // console.log(urlDatabase.);
+  res.render("urls_show", templateVars);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
